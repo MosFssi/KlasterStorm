73bba6d72ecdca828b8860fc73f741e9dfb468f5
MosFssi <45297179+MosFssi@users.noreply.github.com>
2018-11-23 22:18:09 +0300

    Add files via upload

---
+++ KlasterStorm/src/klaster/main/klasterstorm.java
@@ -0,0 +1,19 @@
+package klaster.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class klasterstorm extends JavaPlugin{
+	
+	public void onEnable() {
+		
+		getLogger().info("enabled! Test by Deda");
+		
+	}
+	
+    public void onDisable() {
+    	
+    	getLogger().info("disable! Test by Deda");
+		
+	}
+
+}
